c67228521da7ff2464a06fe01334f65b85784f60
Q-tip <newton21989@gmail.com>
2023-12-05 01:14:18 -0600

    Day 1 part 2 WIP This does everything it's supposed to, but AoC says the answer is wrong

--- day01.py
+++ day01.py
@@ -12,19 +12,52 @@ else:
     lines = read.splitlines(0)
     f.close()
 
-total = 0
+def numStrReplace(numberString):
+  matches = re.findall(r"(one|two|three|four|five|six|seven|eight|nine)", numberString)
+  while matches.__len__() > 0:
+    match = matches.pop(0)
+    if match == "one":
+      num = "1"
+    elif match == "two":
+      num = "2"
+    elif match == "three":
+      num = "3"
+    elif match == "four":
+      num = "4"
+    elif match == "five":
+      num = "5"
+    elif match == "six":
+      num = "6"
+    elif match == "seven":
+      num = "7"
+    elif match == "eight":
+      num = "8"
+    elif match == "nine":
+      num = "9"
 
-for line in lines:
-  match = re.findall(r"^\D*(\d)|.*(\d).*$", line)
+    numberString = re.sub(match, num, numberString, 1)
+  
+  return numberString
+
+def getData(string):
+  match = re.findall(r"(\d)|(\d)", string)
   firstDigit = match[0][0]
   if(match.__len__() > 1):
-    secondDigit = match[1][1]
+    secondDigit = match[match.__len__() - 1][0]
   else:
     secondDigit = match[0][0]
 
-  strNum = f"{firstDigit}{secondDigit}"
+  return f"{firstDigit}{secondDigit}"
 
-  intNum = int(strNum)
-  total += intNum
+def getTotal(lines):
+  total = 0
+  for line in lines:
+    line = numStrReplace(line)
+    strData = getData(line)
 
-print(f"Total = {total}")
+    intNum = int(strData)
+    total += intNum
+
+  return total
+
+print(f"Total = {getTotal(lines)}")
